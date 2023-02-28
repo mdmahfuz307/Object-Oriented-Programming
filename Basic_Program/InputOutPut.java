19e3077cc0d4d58c48be8b2706f1e3c0522135e7
mdmahfuz307 <njnirob2019@gmail.com>
2023-02-28 22:15:22 +0600

    Add files via upload

---
+++ Basic_Program/InputOutPut.java
@@ -0,0 +1,10 @@
+package Basic_Program;
+import java.util.Scanner;
+
+public class InputOutPut {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in); // Create a Scanner object
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
