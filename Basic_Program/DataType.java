307570d7dc847924381251432b81fea80a306a4b
mdmahfuz307 <njnirob2019@gmail.com>
2023-03-17 00:12:15 +0600

    Add files via upload

---
+++ Basic_Program/DataType.java
@@ -0,0 +1,12 @@
+package Basic_Program;
+
+public class DataType {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 25;
+
+        int mul = a * b;
+        System.out.println(mul);
+    }
+
+}
--- Basic_Program/hello.java
+++ Basic_Program/hello.java
@@ -1,1 +1,7 @@
-{\rtf1}
+class hello {
+    public  static  void main(String[] args){
+        System.out.println("Hello");
+    }
+
+
+}
