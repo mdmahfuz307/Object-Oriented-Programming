9db8cbea7d72272df644a95bad82194f0ddaf2c7
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-07-28 20:20:27 +0600

    Update DataType.java

--- Basic_Program/DataType.java
+++ Basic_Program/DataType.java
@@ -5,6 +5,17 @@ public class DataType {
         int a = 10;
         int b = 25;
 
+        /*
+         byte - 1[-128 to 127]
+         short - 2
+         int - 4
+         long - 8
+         float - 4
+         double - 8
+         char - 2
+        bollean - 1 true/false
+        */
+
         int mul = a * b;
         System.out.println(mul);
     }
