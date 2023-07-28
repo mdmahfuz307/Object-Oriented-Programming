9dfd0770f7ef58ae990c24654a0d53fabeed8177
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-07-28 20:26:57 +0600

    PrimativeDataTypes.java

---
+++ Basic_Program/PrimativeDataTypes.java
@@ -0,0 +1,15 @@
+package Basic_Program;
+
+public class PrimativeDataTypes {
+    public static void main(String[] args){
+        //Primative Data Types
+        byte age = 12;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = false;
+    }
+
+}
+
