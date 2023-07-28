8f68d6a26594983975b0ab960a485412aa84d94b
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-07-28 20:12:10 +0600

    variables.java

---
+++ Basic_Program/variables.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class variables {
+    public static void main(String[] args)
+    {
+        //variables
+        String name = "Nirob";
+        String neighbour = "Rakib";
+        int age = 30;
+    }
+}
