5ca133911e33e2c675ab113e00feb8a2c0c3bec3
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2023-07-28 20:34:07 +0600

    Non_Primitive_Typs.java

---
+++ Basic_Program/Non_Primitive_Typs.java
@@ -0,0 +1,7 @@
+public class Non_Primitive_Typs {
+    public static void main(String[] args){
+        //Non-Primitiv-Data-Types
+        String name = "Nirob";
+        System.out.println(name.length());
+    }
+}
