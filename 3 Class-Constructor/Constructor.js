07e22d6f8219a36f6b4dd4f5420718f8659f7a2b
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-16 23:43:06 +0600

    Constructor

---
+++ 3 Class-Constructor/Constructor.js
@@ -0,0 +1,15 @@
+// Constructor name should be Constructor
+// Constructor execute automatically when object is created
+// Constructor can take parameter
+// Constructor method con't return any result
+
+class Person{
+
+  constructor() {
+    console.log("I am a Constructor Method")
+  }
+
+}
+
+
+let PersonObj = new Person();
